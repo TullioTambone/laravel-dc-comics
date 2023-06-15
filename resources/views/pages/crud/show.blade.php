83b0ba23c97e5f5836bfb1129390fd60feeabb81
TullioTambone <tulliotambone97@gmail.com>
2023-06-15 16:22:41 +0200

    aggiusti finali - fine

--- resources/views/pages/crud/show.blade.php
+++ resources/views/pages/crud/show.blade.php
@@ -9,22 +9,33 @@
 
 <div class="container-fluid" id="card-section">
     <div class="container card-section p-5">
-        <div class="my-card p-3 col-12 d-flex justify-content-center flex-column align-items-center text-white">
-            <h3 class="mt-1">{{$comic['title']}}</h3>
-            <img src="{{$comic['thumb']}}" alt="" style="width:150px">
-            <p class="mt-2">{{$comic['description']}}</p>
+        <div class="row">
+            <div class="my-card p-3 col-6 d-flex justify-content-center flex-column align-items-center text-white">
+                <img src="{{$comic['thumb']}}" alt="" style="">
+            </div>
+            
+            <div class="col-6 text-start text-white">            
+                <h3 class="mt-1">{{$comic['title']}}</h3>
+                <span class="mt-2 d-block">{{$comic['description']}}</span>
+                <span class="mt-1 d-block">{{ $comic['price'] }}</span>
+                <span class="mt-1 d-block">{{ $comic['sale_date'] }}</span>
+                <span class="mt-1 d-block">{{ $comic['type'] }}</span>
+                <span class="mt-1 d-block">{{ $comic['artists'] }}</span>
+                <span class="mt-1 d-block">{{ $comic['writers'] }}</span>
+
+                <div class="d-flex align-items-center mt-4">
+                    <a href="{{ route('comics.edit', $comic) }}">
+                        <button class="btn btn-primary">modifica</button>
+                    </a>
+            
+                    <form action="{{ route('comics.destroy', $comic) }}" method="POST" class="ms-3">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-danger" type="submit">elimina</button>
+                    </form>
+                </div>
+            </div>
         </div>
-
-        <a href="{{ route('comics.edit', $comic) }}">
-            <button class="btn btn-primary">modifica</button>
-        </a>
-
-        <form action="{{ route('comics.destroy', $comic) }}" method="POST">
-            @csrf
-            @method('DELETE')
-            <button class="btn btn-danger">elimina</button>
-        </form>
-
     </div>
 </div>
 
